911fb022d66fa8698149eec8ecba2208602ff239
“Phil <philsmithies@gmail.com>
2021-05-23 18:13:21 +0200

    crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
@@ -10,9 +20,18 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
   end
 
+  def show 
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
 
   private
   def post_params
