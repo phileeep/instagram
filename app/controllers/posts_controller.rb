4b9b250887812fd5a3bc645af63f3099e68b8ad0
“Phil <philsmithies@gmail.com>
2021-05-23 16:59:14 +0200

    styles and forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
 
-  def index
+  def new
+    @post = Post.new
   end
 
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 end
